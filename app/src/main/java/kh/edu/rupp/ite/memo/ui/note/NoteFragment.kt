44ea149f6bc0c028505c5483b654c6902d8eeab7
Ea Veasna <eaveasna52@gmail.com>
2023-12-15 11:55:08 +0700

    feat(note) : add initial data and pass data between note , home

--- app/src/main/java/kh/edu/rupp/ite/memo/ui/note/NoteFragment.kt
+++ app/src/main/java/kh/edu/rupp/ite/memo/ui/note/NoteFragment.kt
@@ -4,17 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.google.gson.Gson
+import dagger.hilt.android.AndroidEntryPoint
 import kh.edu.rupp.ite.memo.R
 import kh.edu.rupp.ite.memo.databinding.FragmentNoteBinding
+import kh.edu.rupp.ite.memo.models.NoteRequest
+import kh.edu.rupp.ite.memo.models.NoteResponse
+import kh.edu.rupp.ite.memo.utils.NetworkResponse
+import kh.edu.rupp.ite.memo.viewmodel.NoteViewModel
 
+@AndroidEntryPoint
 class NoteFragment : Fragment() {
 
     private var _binding: FragmentNoteBinding? = null
     private val binding get() = _binding!!
+    private val noteViewModel by viewModels<NoteViewModel>()
+    private var note: NoteResponse? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,43 +32,72 @@ class NoteFragment : Fragment() {
     ): View? {
         _binding = FragmentNoteBinding.inflate(inflater, container, false)
         return binding.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setInitialData()
+        bindHandlers()
+        bindObservers()
 
-        // Spinner setup
-        val spinnerOptions = arrayOf("Done", "In Progress", "Not Started")
-        val spinnerAdapter =
-            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, spinnerOptions)
-        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        binding.spinner.adapter = spinnerAdapter
+    }
 
-        // Spinner item selection listener
-        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                val selectedStatus = spinnerOptions[position]
-                // Handle the selected status as needed
+    private fun bindObservers() {
+        noteViewModel.statusLiveData.observe(viewLifecycleOwner, Observer {
+            when (it) {
+                is NetworkResponse.Success -> {
+                    findNavController().popBackStack()
+                }
+                is NetworkResponse.Error -> {
+
+                }
+                is NetworkResponse.Loading -> {
+
+                }
             }
+        })
+    }
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                // Do nothing here
+    private fun bindHandlers() {
+        binding.btnDelete.setOnClickListener {
+            note?.let { noteViewModel.deleteNote(it!!._id) }
+        }
+        binding.apply {
+            btnSubmit.setOnClickListener {
+                val title = txtTitle.text.toString()
+                val description = txtDescription.text.toString()
+                val noteRequest = NoteRequest(title, description)
+                if (note == null) {
+                    noteViewModel.createNote(noteRequest)
+                } else {
+                    noteViewModel.updateNote(note!!._id, noteRequest)
+                }
             }
         }
-
-        // Back button click listener
         binding.backButtom.setOnClickListener {
-            it.findNavController().navigate(R.id.action_noteFragment_to_loadingFragment)
+            it.findNavController().navigate(R.id.action_noteFragment_to_homeFragment)
         }
     }
+
+    private fun setInitialData() {
+        val jsonNote = arguments?.getString("note")
+        if (jsonNote != null) {
+            note = Gson().fromJson<NoteResponse>(jsonNote, NoteResponse::class.java)
+            note?.let {
+                binding.txtTitle.setText(it.title)
+                binding.txtDescription.setText(it.description)
+            }
+        }
+        else{
+//            binding.addEditText.text = resources.getString(R.string.add_note)
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }
